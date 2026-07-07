d64fa8cb9f3c1ac28f20dcc29d922fa7c0e67d9c
komaweb <marimoxinzhong@gmail.com>
2026-07-07 23:34:19 +0900

    Create historyTitle.js

---
+++ js/ui/components/historyTitle.js
@@ -0,0 +1,37 @@
+import { createText } from "./text.js";
+
+export function createHistoryTitle(item){
+
+    if(item.deleted){
+
+        return createText({
+
+            text:"🗑 取り消し済み",
+
+            classes:[
+
+                "history-title",
+
+                "deleted-title"
+
+            ]
+
+        });
+
+    }
+
+    if(!item.title){
+
+        return null;
+
+    }
+
+    return createText({
+
+        text:item.title,
+
+        classes:["history-title"]
+
+    });
+
+}
