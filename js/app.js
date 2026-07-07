43a484cee789e48d0cb6de0095f619ec3062f11c
komaweb <marimoxinzhong@gmail.com>
2026-07-07 22:59:38 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
+import { loadPage } from "./ui/navigation.js";
+
 import { updateBalance } from "./ui/balance.js";
 import { renderHistory } from "./ui/historyView.js";
 import { initializeButtons } from "./ui/buttons.js";
+
+async function initializeMoneyPage(){
+
+    await loadPage("money");
+
+    refresh();
+
+    initializeButtons(refresh);
+
+}
 
 function refresh(){
 
@@ -12,8 +24,6 @@ function refresh(){
 
 document.addEventListener("DOMContentLoaded",()=>{
 
-    refresh();
-
-    initializeButtons(refresh);
+    initializeMoneyPage();
 
 });
