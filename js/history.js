715cf381fe5657c0c04d32a02abcf2c56a660cb6
komaweb <marimoxinzhong@gmail.com>
2026-07-07 20:55:48 +0900

    Update history.js

--- js/history.js
+++ js/history.js
@@ -1,8 +1,28 @@
-export const historyData = [];
+import { saveHistory, loadHistory } from "./storage.js";
+
+export const historyData = loadHistory();
 
 export function addHistory(data){
 
     historyData.unshift(data);
+
+    saveHistory(historyData);
+
+}
+
+export function removeHistory(id){
+
+    const index = historyData.findIndex(item=>item.id===id);
+
+    if(index===-1){
+
+        return;
+
+    }
+
+    historyData.splice(index,1);
+
+    saveHistory(historyData);
 
 }
 
