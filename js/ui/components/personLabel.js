1bb772a1954b3a547157bdf5ed4c1c5c02e3ff45
komaweb <marimoxinzhong@gmail.com>
2026-07-08 18:39:27 +0900

    Update createPersonLabel to use createPersonView
    Refactor createPersonLabel to use createPersonView for displaying person details.

--- js/ui/components/personLabel.js
+++ js/ui/components/personLabel.js
@@ -1,11 +1,22 @@
+import { createPersonView } from "./personView.js";
+
 export function createPersonLabel(person){
 
     const element = document.createElement("div");
 
     element.className = "history-detail";
 
-    element.textContent =
-        `${person.icon} ${person.name}が支払いました`;
+    element.appendChild(
+
+        createPersonView(person)
+
+    );
+
+    element.append(
+
+        "が支払いました"
+
+    );
 
     return element;
 
