f8711c9eb0f08c6601a3b919a5c95fe1c4c3f71a
komaweb <marimoxinzhong@gmail.com>
2026-07-08 20:27:16 +0900

    Update personLabel.js

--- js/ui/components/personLabel.js
+++ js/ui/components/personLabel.js
@@ -1,21 +1,51 @@
-import { createPersonText } from "./personText.js";
-
 export function createPersonLabel(person){
 
-    return createPersonText(
+    const element = document.createElement("div");
 
-        person,
+    element.className = "history-detail";
 
-        "が支払いました",
+    const icon = document.createElement("div");
 
-        {
+    if(person.image){
 
-            layout:"row",
+        const image = document.createElement("img");
 
-            size:"small"
+        image.className =
 
-        }
+            "history-person-icon";
 
-    );
+        image.src = person.image;
+
+        image.alt = person.name;
+
+        icon.appendChild(image);
+
+    }else{
+
+        icon.className =
+
+            "history-person-placeholder";
+
+        icon.style.backgroundColor =
+
+            person.color;
+
+    }
+
+    element.appendChild(icon);
+
+    const text = document.createElement("span");
+
+    text.className =
+
+        "history-person-text";
+
+    text.textContent =
+
+        `${person.name}が支払いました`;
+
+    element.appendChild(text);
+
+    return element;
 
 }
